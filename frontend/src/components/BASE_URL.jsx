b049ab06904d283bdad0c9af61e6151cc66c4466
Prince Kumar <princekumar5252@gmail.com>
2025-12-21 15:26:41 +0530

    Add fallback URL to BASE_URL configuration

--- frontend/src/components/BASE_URL.jsx
+++ frontend/src/components/BASE_URL.jsx
@@ -1,2 +1,4 @@
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL =
+  import.meta.env.VITE_BASE_URL ||
+  "https://vechile-validator-backend-im0r.onrender.com/";
 export default BASE_URL;
